1079d011487bfdb926bb9c8cbf941b3e53c41a74
Joshua Gottlieb <Bombgh@gmail.com>
2022-12-05 01:39:35 -0800

    Onlyfans online checker fix

--- OnlyCas.py
+++ OnlyCas.py
@@ -3,8 +3,6 @@ from selenium.webdriver.common.by import By
 from SeleniumDriverCreator import SeleniumDriverCreator
 
 class OnlyCas:
-
-
     def __init__(self):
         self.CAS_ONLY_URL = "https://onlyfans.com/badkittycass"
 
@@ -12,12 +10,11 @@ class OnlyCas:
         driverCreator = SeleniumDriverCreator()
         driver = driverCreator.createDriver()
         driver.get(self.CAS_ONLY_URL)
-        await asyncio.sleep(10)
-        online = driver.find_elements(By.XPATH, '//*[@id="content"]/div[1]/div/div[2]/div/div[2]/div[1]/a/span')
+        asyncio.sleep(10)
+        online = driver.find_elements(By.XPATH, '/html/body/div/div[2]/main/div[1]/div[1]/div/div[2]/div/div[2]/div[1]/a/span')
         driver.quit()
         isOnline = False
         if len(online) > 0:
             isOnline = True
 
         return isOnline
-
