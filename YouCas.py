14c9e6460b3c3970bdec6725edb404b1436ca28e
Joshua Gottlieb <Bombgh@gmail.com>
2023-01-19 23:35:03 -0600

    fixed yt for real this time maybe

--- YouCas.py
+++ YouCas.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+import json
 
 class YouCas:
     def __init__(self):
@@ -10,11 +12,20 @@ class YouCas:
         page = requests.get(self.CAS_YT_URL, cookies={'CONSENT': 'YES+42'})
         soup = BeautifulSoup(page.content, "html.parser")
         live = soup.find("link", {"rel": "canonical"})
-        if live: 
-            #print("Cass is streaming on youtube")
+        scripts = soup.find_all('script')
+        ytJson = str(scripts).split('var ytInitialPlayerResponse = ')
+        # with open('jsonText', 'w', encoding="utf-8") as f:
+        #     f.write(ytJson[1])
+        splitJson = str(ytJson[1]).split(";</script>")
+        compJson = json.loads(splitJson[0])
+        status = compJson["playabilityStatus"]["status"]
+        if live and status != "LIVE_STREAM_OFFLINE": 
+            print("Cass is streaming on youtube")
             online = True
-        #else:
-            #print("Cass is not streaming on youtube")
+        else:
+            print("Cass is not streaming on youtube")
         
         return online
 
+yt = YouCas()
+online = yt.isCassOnline()
