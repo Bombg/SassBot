d0c6678e77f29150870d7c559f2f8fe23b373d0c
Joshua Gottlieb <Bombgh@gmail.com>
2023-01-20 14:02:19 -0600

    fans fix again

--- FansCas.py
+++ FansCas.py
@@ -8,13 +8,13 @@ class FansCas:
         self.CAS_FANS_URL = "https://fansly.com/user443986457938374656"
         #https://fansly.com/live/user443986457938374656
 
-    def isCassOnline(self):
+    async def isCassOnline(self):
         driverCreator = SeleniumDriverCreator()
         driver = driverCreator.createDriver()
         driver.get(self.CAS_FANS_URL)
-        #await asyncio.sleep(10)
-        time.sleep(10)
-        driver.get_screenshot_as_file("Fansscreenshot.png")
+        await asyncio.sleep(10)
+        #time.sleep(10)
+        #driver.get_screenshot_as_file("Fansscreenshot.png")
         online = driver.find_elements(By.XPATH, '/html/body/app-root/div/div[1]/div/app-profile-route/div/div/div/div[1]/div[2]/div[1]/app-account-avatar/div')
         driver.quit()
         isOnline = False
