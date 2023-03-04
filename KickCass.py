bfad3b077a81737a023b374f5a9060f81074a90a
Joshua Gottlieb <Bombgh@gmail.com>
2023-03-04 02:05:17 -0600

    async for kick

--- KickCass.py
+++ KickCass.py
@@ -7,13 +7,13 @@ from SeleniumDriverCreator import SeleniumDriverCreator
 class KickCass:
     def __init__(self):
         self.CAS_KICK_URL = 'https://kick.com/kittycass'
-    def isCassOnline(self):
+    async def isCassOnline(self):
         driverCreator = SeleniumDriverCreator()
         driver = driverCreator.createDriver()
         driver.get(self.CAS_KICK_URL)
-        #await asyncio.sleep(10)
-        time.sleep(10)
-        #driver.get_screenshot_as_file("Fansscreenshot.png")
+        await asyncio.sleep(10)
+        #time.sleep(10)
+        driver.get_screenshot_as_file("Fansscreenshot.png")
         online = driver.find_elements(By.XPATH, '/html/body/div/div[1]/div/div/div/div[2]/div[2]/div/div/div[1]/div/div[1]/div[2]/div/div/div[1]/div[1]/div/div[2]')
         driver.quit()
         isOnline = False
