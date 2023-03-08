c9c98e5b092d4b615f407ebd8b0a7664c37bebbb
Joshua Gottlieb <Bombgh@gmail.com>
2023-03-07 23:07:29 -0600

    longer wait for chatur

--- ChaturCas.py
+++ ChaturCas.py
@@ -3,8 +3,6 @@ from selenium.webdriver.common.by import By
 from SeleniumDriverCreator import SeleniumDriverCreator
 
 class ChaturCas:
-
-
     def __init__(self):
         self.CAS_CHATUR_URL = "https://chaturbate.com/badkittycass/"
 
@@ -12,11 +10,11 @@ class ChaturCas:
         driverCreator = SeleniumDriverCreator()
         driver = driverCreator.createDriver()
         driver.get(self.CAS_CHATUR_URL)
-        await asyncio.sleep(10)
+        await asyncio.sleep(15)
         button = driver.find_elements(By.XPATH, '//*[@id="close_entrance_terms"]')
         if len(button) > 0:
             button[0].click()
-        await asyncio.sleep(6)
+        await asyncio.sleep(10)
         online = driver.find_elements(By.XPATH, '//*[@id="vjs_video_3"]/div[4]/div[9]')
         driver.quit()
         isOnline = False
