e28ae9d70db933931a408aa67d11ae9609d8a5f7
Joshua Gottlieb <Bombgh@gmail.com>
2023-04-25 21:41:18 -0500

    Kick fix again

--- checkers/KickCass.py
+++ checkers/KickCass.py
@@ -13,16 +13,11 @@ class KickCass:
         driver.get(self.CAS_KICK_URL)
         await asyncio.sleep(5)
         driver.get_screenshot_as_file("Kickscreenshot.png")
-        button = driver.find_elements(By.XPATH, '/html/body/div/div[2]/div/div/div/div[2]/div[2]/div/div[2]/div[2]/div/div[4]/button[2]')
-        if len(button) > 0:
-            button[0].click()
-            offlineOwnerAvatar = driver.find_elements(By.XPATH, '/html/body/div/div[2]/div/div/div/div[2]/div[2]/div/div/div[1]/div/div[2]/div[1]/div')
-            online = driver.find_elements(By.XPATH, '/html/body/div/div[2]/div/div/div/div[2]/div[2]/div/div/div[1]/div/div[1]/div[2]/div/div/div[1]/div[1]/div/div[2]')
-            driver.quit()
-            if len(online) > 0:
-                isOnline = True
-            elif len(offlineOwnerAvatar) < 1:
-                isOnline = 3
-        else:
-            print("didn't find kick button, super fail, fix me pls")
+        offlineOwnerAvatar = driver.find_elements(By.XPATH, '/html/body/div/div[2]/div/div/div/div[2]/div[2]/div/div/div[1]/div/div[2]/div[1]/div')
+        online = driver.find_elements(By.XPATH, '/html/body/div/div[2]/div/div/div/div[2]/div[2]/div/div[2]/div[2]/div/div[4]/button[2]')
+        driver.quit()
+        if len(online) > 0:
+            isOnline = True
+        elif len(offlineOwnerAvatar) < 1:
+            isOnline = 3
         return isOnline
