7df3dca28235b8dd8312a1223bd00cab64397309
Joshua Gottlieb <Bombgh@gmail.com>
2023-04-25 21:34:48 -0500

    kick fix

--- checkers/KickCass.py
+++ checkers/KickCass.py
@@ -13,19 +13,16 @@ class KickCass:
         driver.get(self.CAS_KICK_URL)
         await asyncio.sleep(5)
         driver.get_screenshot_as_file("Kickscreenshot.png")
-        offlineOwnerAvatar = driver.find_elements(By.XPATH, '/html/body/div/div[1]/div/div/div/div[2]/div[2]/div/div/div[1]/div/div[2]/div[1]/div')
-        online = driver.find_elements(By.XPATH, '/html/body/div/div[1]/div/div/div/div[2]/div[2]/div/div/div[1]/div/div[1]/div[2]/div/div/div[1]/div[1]/div/div[2]')
-        driver.quit()
-        if len(online) > 0:
-            isOnline = True
-        elif len(offlineOwnerAvatar) < 1:
-            isOnline = 3
+        button = driver.find_elements(By.XPATH, '/html/body/div/div[2]/div/div/div/div[2]/div[2]/div/div[2]/div[2]/div/div[4]/button[2]')
+        if len(button) > 0:
+            button[0].click()
+            offlineOwnerAvatar = driver.find_elements(By.XPATH, '/html/body/div/div[2]/div/div/div/div[2]/div[2]/div/div/div[1]/div/div[2]/div[1]/div')
+            online = driver.find_elements(By.XPATH, '/html/body/div/div[2]/div/div/div/div[2]/div[2]/div/div/div[1]/div/div[1]/div[2]/div/div/div[1]/div[1]/div/div[2]')
+            driver.quit()
+            if len(online) > 0:
+                isOnline = True
+            elif len(offlineOwnerAvatar) < 1:
+                isOnline = 3
+        else:
+            print("didn't find kick button, super fail, fix me pls")
         return isOnline
-
-# async def test():
-#     kick = KickCass()
-#     task = asyncio.create_task(kick.isCassOnline())
-#     isOnline = await task
-#     print(isOnline)
-
-# task = asyncio.run(test())
