0a091da0a135bf465a8aa15d0299669e07618c2a
Joshua Gottlieb <Bombgh@gmail.com>
2023-05-14 23:40:28 -0500

    twitter grabber changes

--- TwitterImageGrabber.py
+++ TwitterImageGrabber.py
@@ -12,7 +12,8 @@ class TwitterImageGrabber:
         driverCreator = SeleniumDriverCreator()
         driver = driverCreator.createDriver()
         driver.get(self.twitterUrl)
-        await asyncio.sleep(5)
+        await asyncio.sleep(8)
+        driver.get_screenshot_as_file("twitterShot.png")
         element = driver.find_elements(By.XPATH, '/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/section/div/div/div[1]/div/div/article/div/div/div[2]/div[2]/div[3]/div/div/div/div/div/div/a/div/div[2]/div/img')
         if len(element) > 0:
             imageSrc = element[0].get_attribute('src')
