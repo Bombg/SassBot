ed8cf3d2c9826ae0783786f5938f86ec699ed61d
Joshua Gottlieb <Bombgh@gmail.com>
2023-05-26 01:12:19 -0500

    fixed kick title grabber

--- checkers/KickCass.py
+++ checkers/KickCass.py
@@ -18,7 +18,7 @@ class KickCass:
         online = driver.find_elements(By.XPATH, '/html/body/div/div[2]/div/div/div/div[2]/div[2]/div/div[2]/div[2]/div/div[4]/button[2]')
         if len(online) > 0:
             isOnline = True
-            titleX = driver.find_elements(By.XPATH, '/html/body/div/div[2]/div/div/div/div[2]/div[2]/div/div/div[1]/div/div[1]/div[2]/div/div/div[1]/div[2]/div[2]/span')
+            titleX = driver.find_elements(By.XPATH, '/html/body/div/div[2]/div/div/div/div[2]/div[2]/div/div/div[1]/div/div[2]/div/div/div/div[1]/div[2]/div[2]/span')
             if len(titleX) > 0:
                 title = titleX[0].get_attribute("innerHTML")
                 title = title.split("<!---->")[0]
