f7ebc915d1682ff788efedcf228fe401dd9b6516
Joshua Gottlieb <Bombgh@gmail.com>
2023-05-26 01:28:22 -0500

    stream status clean up and bug fix

--- plugins/commands.py
+++ plugins/commands.py
@@ -36,17 +36,13 @@ async def streamStatus(ctx: tanjun.abc.Context) -> None:
     if globals.kickFalse <= 0:
         streamingOn = streamingOn + "Kick, "
     if streamingOn == "":
-        await ctx.respond("Cass isn't currently streaming, but check out her offline content! \n Links: https://linktr.ee/kitty_cass_")
-    else:
-        await ctx.respond("Cass is currently streaming on: \n " + streamingOn + "\n Links: https://linktr.ee/kitty_cass_")
-    if globals.online:
-        asyncResult = pool.apply_async(StaticMethods.timeToHoursMinutes,(lastOnline,))
-        hours, minutes = asyncResult.get()
-        await ctx.respond("Cass has been online for H:" + str(hours) + " M:" + str(minutes))
-    else:
         asyncResult = pool.apply_async(StaticMethods.timeToHoursMinutes,(lastOffline,))
         hours, minutes = asyncResult.get()
-        await ctx.respond("Cass has been offline for H:" + str(hours) + " M:" + str(minutes))
+        await ctx.respond("Cass isn't currently streaming and has been offline for H:" + str(hours) + " M:" + str(minutes) + ", but check out her offline content! \n Links: https://linktr.ee/kitty_cass_")
+    else:
+        asyncResult = pool.apply_async(StaticMethods.timeToHoursMinutes,(lastOnline,))
+        hours, minutes = asyncResult.get()
+        await ctx.respond("Cass is currently streaming on: \n " + streamingOn + " and has been online for" + str(hours) + " M:" + str(minutes) + "\n Links: https://linktr.ee/kitty_cass_")
     tHours, tMinutes = StaticMethods.timeToHoursMinutesTotalTime(totalStreamTime)
     date = datetime.fromtimestamp(1684210200)
     await ctx.respond("Cass has streamed a grand total of H:" + str(tHours) + " M:" + str(tMinutes) + " since records have been kept starting on " + str(date)) 
