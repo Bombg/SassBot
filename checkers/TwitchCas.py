3f103a20aba95c1b210f7095a5ea11b12db34329
Joshua Gottlieb <Bombgh@gmail.com>
2023-06-21 20:58:22 -0500

    twitch bug fix

--- checkers/TwitchCas.py
+++ checkers/TwitchCas.py
@@ -4,12 +4,13 @@ def isCassOnline(channelName):
     url = "https://gql.twitch.tv/gql"
     query = "query {\n  user(login: \""+ channelName +"\") {\n    stream {\n      id\n    }\n  }\n}"
     title = "placeholder twitch title"
-    thumbUrl = ""
+    thumbUrl = f'https://static-cdn.jtvnw.net/previews-ttv/live_user_{channelName}-1920x1080.jpg'
     isOnline = False
     icon = 'images/errIcon.png'
+    req = requests.request("POST", url, json={"query": query, "variables": {}}, headers={"client-id": "kimne78kx3ncx6brgo4mv6wki5h1ko"})
     try:
-        if requests.request("POST", url, json={"query": query, "variables": {}}, headers={"client-id": "kimne78kx3ncx6brgo4mv6wki5h1ko"}).json()["data"]["user"]["stream"]:
+        if req.json()["data"]["user"]["stream"]:
             isOnline = True
-        return isOnline, title,thumbUrl
+        return isOnline, title,thumbUrl, icon
     except(TypeError):
         return isOnline, title, thumbUrl, icon
