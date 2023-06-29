fab00ce14b231d067f625b8771258ca216e9f2d2
Joshua Gottlieb <Bombgh@gmail.com>
2023-06-29 01:56:54 -0500

    kick checker fix

--- checkers/Kick.py
+++ checkers/Kick.py
@@ -13,11 +13,14 @@ def isModelOnline(kickUserName):
     driver.get(apiUrl)
     time.sleep(3)
     content = driver.page_source.split('<body>')
-    jsonText = content[1].split('</body></html>')
-    results = json.loads(jsonText[0])
-    if results['livestream']:
-        isOnline = True
-        title = results['livestream']['session_title']
-        thumbUrl = results['livestream']['thumbnail']['url']+ "?" + str(int(time.time()))
-        icon = results['user']['profile_pic']
+    if len(content) < 2:
+        print("error with kick checker. user is banned or wrong username supplied")
+    else:
+        jsonText = content[1].split('</body></html>')
+        results = json.loads(jsonText[0])
+        if results['livestream']:
+            isOnline = True
+            title = results['livestream']['session_title']
+            thumbUrl = results['livestream']['thumbnail']['url']+ "?" + str(int(time.time()))
+            icon = results['user']['profile_pic']
     return isOnline, title, thumbUrl, icon
