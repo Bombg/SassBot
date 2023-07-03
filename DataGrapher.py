c71f4ad71256fd23af409a3da48bcb86369f7e71
Joshua Gottlieb <Bombgh@gmail.com>
2023-07-03 01:18:06 -0500

    adding again? I'm messing up things now

--- DataGrapher.py
+++ DataGrapher.py
@@ -7,21 +7,41 @@ def createUserDayGraph(date: str) -> None:
     yyyyDashMmDashDd = date
     presencesDict = db.getPresenceDay(yyyyDashMmDashDd)
     x = []
-    y = []
+    yTotalUsers = []
+    yDnd = []
+    yOnline = []
+    yIdle = []
     for k, v in presencesDict.items():
         if v:
             x.append(k)
-            newVal = 0
+            totalUsers = 0
+            dnd = 0
+            online = 0
+            idle = 0
             for keys, vals in v.items():
-                newVal += vals
-            y.append(newVal)
-    plt.figure(figsize=(15, 4))
-    plt.plot(x,y)
+                totalUsers += vals
+                if keys == 'dnd':
+                    dnd += vals
+                elif keys == 'online':
+                    online += vals
+                elif keys == 'idle':
+                    idle += vals
+            yTotalUsers.append(totalUsers)
+            yDnd.append(dnd)
+            yOnline.append(online)
+            yIdle.append(idle)
+    plt.figure(figsize=(15, 5))
+    plt.plot(x,yTotalUsers, label = "Total users(logged in to discord)")
+    plt.plot(x,yDnd, label ="dnd", color = 'red')
+    plt.plot(x,yOnline, label = "online", color = "green")
+    plt.plot(x, yIdle, label = "idle", color = "orange")
+    plt.legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
     ax = plt.gca()
     temp = ax.xaxis.get_ticklabels()
     temp = list(set(temp) - set(temp[::6]))
     for label in temp:
         label.set_visible(False)
     if not os.path.exists("graphs"):
-        os.makedirs("graphs")    
+        os.makedirs("graphs")
+    plt.tight_layout()    
     plt.savefig(f"graphs/{yyyyDashMmDashDd}.png")
