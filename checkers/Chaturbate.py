f587b28bfe0ffea92c4568182934c5e9ca448bf8
Joshua Gottlieb <Bombgh@gmail.com>
2023-07-16 20:17:49 -0500

    cb fix to catch error if api no respond

--- checkers/Chaturbate.py
+++ checkers/Chaturbate.py
@@ -2,18 +2,22 @@ import time
 from selenium.webdriver.common.by import By
 import requests
 from Constants import Constants
+import json.decoder 
 
 def isModelOnline(cbApiUrl):
     isOnline = False
     onlineModels = requests.get(cbApiUrl)
     time.sleep(3)
-    results = onlineModels.json()["results"]
-    title = "placeholder cb title"
-    thumbUrl = ""
-    icon = 'images/errIcon.png'
-    for result in results:
-        if result['username'] == Constants.cbUserName:
-            isOnline = True
-            title = result['room_subject']
-            thumbUrl = result['image_url'] + "?" + str(int(time.time()))
+    try:
+        results = onlineModels.json()["results"]
+        title = "placeholder cb title"
+        thumbUrl = ""
+        icon = 'images/errIcon.png'
+        for result in results:
+            if result['username'] == Constants.cbUserName:
+                isOnline = True
+                title = result['room_subject']
+                thumbUrl = result['image_url'] + "?" + str(int(time.time()))
+    except json.decoder.JSONDecodeError:
+        print("cb api didn't respond")
     return isOnline, title, thumbUrl, icon
