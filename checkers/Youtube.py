20a3b7b453232afe57dfc54cc962f4ede108b47f
Joshua Gottlieb <Bombgh@gmail.com>
2023-07-17 14:23:09 -0500

    refactor yt checker

--- checkers/Youtube.py
+++ checkers/Youtube.py
@@ -11,19 +11,36 @@ def isModelOnline(ytUrl):
     soup = BeautifulSoup(page.content, "html.parser")
     live = soup.find("link", {"rel": "canonical"})
     scripts = soup.find_all('script')
-    ytJson = str(scripts).split('var ytInitialPlayerResponse = ')
-    ytJson2 = str(scripts).split('var ytInitialData = ')
-    try:
-        splitJson = str(ytJson[1]).split(";</script>")
-        splitJson2 = str(ytJson2[1]).split(";</script>")
-        iconJson = json.loads(splitJson2[0])
-        compJson = json.loads(splitJson[0])
-        status = compJson["playabilityStatus"]["status"]
-        title = compJson["videoDetails"]['title']
-        thumbUrl = compJson['videoDetails']['thumbnail']['thumbnails'][4]['url']
-        icon = iconJson['contents']['twoColumnWatchNextResults']['results']['results']['contents'][1]['videoSecondaryInfoRenderer']['owner']['videoOwnerRenderer']['thumbnail']['thumbnails'][0]['url']
+    liveJson = getLiveJson(scripts)
+    if liveJson:
+        iconJson = getIconJson(scripts)
+        status = liveJson["playabilityStatus"]["status"]
+        title = liveJson["videoDetails"]['title']
+        thumbUrl = liveJson['videoDetails']['thumbnail']['thumbnails'][4]['url']
         if live and status != "LIVE_STREAM_OFFLINE": 
             online = True
-    except IndexError:
-        pass    
+        if iconJson:
+            icon = iconJson['contents']['twoColumnWatchNextResults']['results']['results']['contents'][1]['videoSecondaryInfoRenderer']['owner']['videoOwnerRenderer']['thumbnail']['thumbnails'][0]['url']
+        else:
+            print("can't get yt icon")
     return online,title, thumbUrl, icon
+
+def getIconJson(scripts):
+    iconJson = 0
+    try:
+        ytJson2 = str(scripts).split('var ytInitialData = ')
+        splitJson2 = str(ytJson2[1]).split(";</script>")
+        iconJson = json.loads(splitJson2[0])
+    except IndexError:
+        pass
+    return iconJson
+
+def getLiveJson(scripts):
+    liveJson = 0
+    try:
+        ytJson = str(scripts).split('var ytInitialPlayerResponse = ')
+        splitJson = str(ytJson[1]).split(";</script>")
+        liveJson = json.loads(splitJson[0])
+    except IndexError:
+        pass
+    return liveJson
