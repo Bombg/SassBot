4ccc47a48f6c3fcc80e699cad0808d99b0c6d8dc
Joshua Gottlieb <Bombgh@gmail.com>
2023-08-12 01:17:37 -0500

    cleaning up bongacams checker

--- checkers/Bongacams.py
+++ checkers/Bongacams.py
@@ -3,7 +3,7 @@ import time
 from bs4 import BeautifulSoup
 import json
 
-def isModelOnlineAlt(bcUserName):
+def isModelOnline(bcUserName):
     title = "placeholder bc title"
     thumbUrl = ''
     isOnline = False
@@ -28,33 +28,6 @@ def isModelOnlineAlt(bcUserName):
             isOnline = not bcJson['chatShowStatusOptions']['isOffline']
             thumbUrl = bcJson['miniProfile']['albums']['albums'][0]['thumbImage']['src']
     return isOnline, title, thumbUrl, icon
-
-# def isModelOnline(bcUserName):
-#     isOnline = False
-#     title = "placeholder bonga title"
-#     thumbUrl = ""
-#     icon = 'images/errIcon.png'
-#     headers = {
-#             "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0",
-#             'Content-Type': 'application/x-www-form-urlencoded',
-#             'Referer': 'https://de.bongacams.net/' + bcUserName,
-#             'Accept': 'application/json, text/javascript, */*; q=0.01',
-#             'X-Requested-With': 'XMLHttpRequest'
-#         }
-#     data = 'method=getRoomData&args%5B%5D=' + bcUserName + '&args%5B%5D=false'
-#     req = requests.post('https://de.bongacams.net/tools/amf.php', data=data, headers=headers)
-#     time.sleep(1)
-#     if req.status_code == 200:
-#         roomJson = req.json()
-#         #print(roomJson)
-#         if 'videoServerUrl' in roomJson['localData']:
-#             bcUserName = roomJson['performerData']['username']
-#             playlist = "https:" + roomJson['localData']['videoServerUrl'] + "/hls/stream_" + bcUserName + "/playlist.m3u8"
-#             reqPl = requests.get(playlist)
-#             print(reqPl.text)
-#             if "#EXTM3U" in reqPl.text:
-#                 isOnline = True
-#     return isOnline, title, thumbUrl, icon
 
 
     
