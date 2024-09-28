88b827a67b0daab36081edf3b0f916e8af66bb15
Joshua Gottlieb <Bombgh@gmail.com>
2024-09-28 02:34:11 -0500

    adding print for display

--- run.py
+++ run.py
@@ -12,11 +12,13 @@ if os.name != "nt":
 
 if __name__ == "__main__":
     if platform.system() == "Linux":
+        print("Opening Display")
         display = Display(visible=0, size=(1080,720))
         display.start()
 
     build_bot().run()
 
     if platform.system() == "Linux":
+        print("Closing Display")
         display.stop()
     
