d5de5d907476ddf2337dd4204fc5fceb0cd74267
Joshua Gottlieb <Bombgh@gmail.com>
2024-09-28 02:19:37 -0500

    changing command run

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
 import platform
+import subprocess
 
 from bot import build_bot
 
@@ -10,5 +11,5 @@ if os.name != "nt":
 
 if __name__ == "__main__":
     if platform.system() == "Linux":
-        os.system('Xvfb $DISPLAY -screen $DISPLAY 1280x1024x16 & export DISPLAY=:99')
+        subprocess.run('Xvfb $DISPLAY -screen $DISPLAY 1280x1024x16 & export DISPLAY=:99')
     build_bot().run()
