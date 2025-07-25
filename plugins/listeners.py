29164a87cdebb7e3aa2f19b603a9e6bb93152020
Joshua Gottlieb <Bombgh@gmail.com>
2025-07-25 09:02:07 -0500

    logging change

--- plugins/listeners.py
+++ plugins/listeners.py
@@ -13,6 +13,7 @@ component = tanjun.Component()
 @component.with_listener(hikari.MessageDeleteEvent)
 async def printDelete(event: hikari.MessageDeleteEvent, rest: alluka.Injected[hikari.impl.RESTClientImpl]):
     try:
+        if event.old_message.author.is_bot: return
         file = open("deletedMessageLogs.txt", 'a')
         date = datetime.fromtimestamp(time.time())
         file.write(f"{date} Author: {event.old_message.author.id}-{event.old_message.author.username} - deleted: {event.old_message.content} or {event.old_message.embeds} in {event.channel_id} \n")
